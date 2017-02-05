77e80525f9597c35aeaa71e0b4973d8980678151
Scary <camscott@fullsail.edu>
2017-02-05 16:42:18 -0600

    fixed db and url model errors

--- models/db.js
+++ models/db.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_SCHEMA,
-  port: process.env.DB_PORT,
+const sequelize = new Sequelize('url-shortener', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: '3306',
   pool: {
     max: 5,
     min: 0,
@@ -15,6 +15,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 const url = sequelize.define('url', {
   id: {
     type: Sequelize.INTEGER,
+    primaryKey: true,
   },
   short_url: {
     type: Sequelize.STRING,
--- models/url.js
+++ models/url.js
@@ -1,5 +1,5 @@
 const db = require('./db');
 
 exports.create = (payload, err, success) => {
-    db.url.create(payload).then(sucess).catch(err);
+    db.url.create(payload).then(success).catch(err);
 }
