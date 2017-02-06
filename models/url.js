3f6a8c9117deabf37aa7c40caa5500d2eaff2ca1
Scary <camscott@fullsail.edu>
2017-02-05 22:02:11 -0600

    added crud

--- models/url.js
+++ models/url.js
@@ -1,5 +1,49 @@
 const db = require('./db');
 
 exports.create = (payload, err, success) => {
-    db.url.create(payload).then(success).catch(err);
+  db.url.create(payload).then(success).catch(err);
+}
+
+// find all urls
+exports.findAll = (err, success) => {
+  db.url.findAll().then(success).catch(err);
+}
+
+// find url by id
+exports.find = (payload, err, success) => {
+  db.url.find({
+    where: {
+      id: payload.id,
+    },
+    // find all relations
+    include: [{
+      all: true,
+      nested: true,
+    }],
+  }).then(success).catch(err);
+}
+
+// update by id
+exports.update = (payload, err, success) => {
+  db.url.update({
+    where: {
+      id: payload.id,
+    }
+  }).then( (existingData) => {
+    existingData.updateAttributes(payload).then(success).catch(err);
+  }).catch(err);
+}
+
+// delete by id
+exports.destroy = (payload, err, success) => {
+  db.url.destroy({
+    where: {
+      id: payload.id,
+    },
+    // find all relations
+    include: [{
+      all: true,
+      nested: true,
+    }],
+  }).then(success).catch(err);
 }
--- routes/api/shortener.js
+++ routes/api/shortener.js
@@ -3,13 +3,46 @@ const url = require('../../models/url');
 module.exports = (express) => {
   const router = express.Router();
 
+  // read all
   router.get('/v1/urls', (req, res) => {
-    res.json({
-      healthy: true,
+    url.findAll( (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
     })
   });
 
-  // create route
+  // read one
+  router.get('/v1/urls/:id', (req, res) => {
+    req.body.id = req.params.id;
+    url.find(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    })
+  });
+
+  // delete
+  router.delete('/v1/urls/:id', (req, res) => {
+    req.body.id = req.params.id;
+    url.destroy(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    })
+  });
+
+  // update
+  router.post('/v1/urls/:id', (req, res) => {
+    req.body.id = req.params.id;
+    url.update(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    })
+  });
+
+  // create a url
   router.post('/v1/urls/', (req, res) => {
     url.create(req.body, (err) => {
       res.status(500).json(err);
