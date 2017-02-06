a0759243ab38677277bb9599fd83f957cf9144b7
Scary <camscott@fullsail.edu>
2017-02-05 22:09:29 -0600

    fixed db model for sequelize and .env

--- models/db.js
+++ models/db.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-const sequelize = new Sequelize('url-shortener', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: '3306',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_SCHEMA,
+  port: process.env.DB_PORT,
   pool: {
     max: 5,
     min: 0,
