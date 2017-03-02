2bf7e7bd294b3fd56e75cbd4d8099b7c80b084ab
Scary <camscott@fullsail.edu>
2017-03-02 17:52:11 -0600

    reinitiating

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: npm install
-web: node src/server.js
--- src/server.js
+++ src/server.js
@@ -1,22 +1,15 @@
+/*
+Cameron Scott
+Deployment of Web Applications
+March 2017
+*/
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-// logging
-let log = require('../modules/debugger');
-log.debugWarn();
-
-// set server port
-const port = process.env.PORT || 3000;
-
-//tell api to use bodyParser.json and urlencoded
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
-
-// use index directory, require routes
-app.use('/', require('../routes')(express));
+// server port is 3000
+let port = 3000;
 
 exports.server = app.listen(port, () => {
   console.log('Server active on port', port);
